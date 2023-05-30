c2e34dc5c84b73f0b0bb1a26733cc5db380a1f42
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2023-05-30 15:47:26 +0300

    fix slider

---
+++ core/swiper.js
@@ -0,0 +1,12 @@
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.slider1', {
+
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      loop: true,
+})
