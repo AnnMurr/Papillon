9e18f18820f335625607aa0b3b8bfba20a14edd8
AnnMurr <annmurina1998@mail.ru>
2023-05-30 16:52:39 +0300

    added sale

--- core/swiper.js
+++ core/swiper.js
@@ -1,6 +1,14 @@
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
-
 const swiper = new Swiper('.slider1', {
+
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      loop: true,
+})
+const swiper3 = new Swiper('.slider3', {
 
     
     navigation: {
